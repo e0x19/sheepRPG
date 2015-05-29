3fc2332c4cb2d9e0e2fe63f732dd5e7fdfcdc663
Razinmore <bsimon@c1mkrzg3dty3.montessorihighschool.org>
2015-05-29 12:50:14 -0400

    class

--- sheepRPG/player.swift
+++ sheepRPG/player.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import UIKit
+
+class player
+{
+    var xCord: CGFloat!
+    var yCord: CGFloat!
+    var size: Double!
+    var lvl: Double!
+    var vtl: Double!
+    var armr: Double!
+    var dfns: Double!
+    var matk: Double!
+    var patk: Double!
+    var spd: Double!
+    var pdmgrst: Double!
+    var mdmgrst: Double!
+    var pdfns: Double!
+    var mdfns: Double!
+    
+    func dmgcalc(ent: player, mdmg: Double, pdmg: Double, dmgflat: Double) -> Double
+    {
+        var pdm = ent.pdmgrst - pdmg
+        var mdm = ent.mdmgrst - mdmg
+        var dmgtot: Double!
+        var dmgfin: Double!
+    
+        
+        
+        return ent.vtl - dmgtot
+    }
+}
+
+
+func initialize()
+{
+    var mc = player()
+    mc.pdmgrst = 26.0
+    mc.mdmgrst = 27.6
+    mc.vtl = 500.0
+    
+    mc.dmgcalc(mc, mdmg: 50, pdmg: 60, dmgflat: 59)
+    println(mc.vtl)
+}
